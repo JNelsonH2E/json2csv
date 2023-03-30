df52567b4de2a8d6b07b614491de19018b55def5
JNelsonH2E <jnelson@h2einc.com>
2023-03-30 15:52:44 -0700

    Json to CSV converter

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+# df = pd.read_json (r'C:\python310\json2csv\tags.json')
+# df.to_csv (r'C:\python310\json2csv\tags.csv', index = None)
+
+
+import json
+import csv
+
+with open('C:\\python310\\json2csv\\tags.json', 'r') as f:
+    data = json.load(f)
+
+rows = []
+
+for item in data:
+    row = {}
+
+    row['name'] = item['name']
+    row['tagType'] = item['tagType']
+    row['tags'] = item['tags']
+
+    row['valueSource'] = item['valueSource']
+    row['opcItemPath'] = item['opcItemPath']
+    row['dataType'] = item['dataType']
+    row['tagType'] = item['tagType']
+    row['opcServer'] = item['opcServer']
+
+    rows.append(row)
+
+
+
+with open('C:\\python310\\json2csv\\tags.csv', 'w', newline='') as f:
+    fieldnames = ['name', 'tagType', 'tags', 'valueSource', 'opcItemPath', 'dataType', 'tagType', 'opcServer']
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for row in rows:
+        writer.writerow(row)
